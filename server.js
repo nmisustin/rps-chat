40fef2f7401802887955fff0f14210df6b495c35
Alex Godwin <alexj.godwin@gmail.com>
2021-05-26 19:57:22 -0400

    server.js and connectivity

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
