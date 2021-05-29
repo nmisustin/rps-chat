67f3b81273567bdbeff6ae8765cfc90d7584ab1e
Alex Godwin <alexj.godwin@gmail.com>
2021-05-29 16:44:07 -0400

    got the server up and running, still need it to connect to the home-routes.js properly

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-const io = require('socket.io')(http);
+const io = require('socket.io');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
