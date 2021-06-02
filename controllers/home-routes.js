3ebf4e658afd6c60723a72c80c7d3a0c8cbbffae
Alex Godwin <alexj.godwin@gmail.com>
2021-06-02 16:42:20 -0400

    separate pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,4 +10,16 @@ router.get('/chat', (req, res) => {
     res.render('chat', {});
 });
 
+router.get('/rockchat', (req, res) => {
+    res.render('rockchat', {});
+});
+
+router.get('/paperchat', (req, res) => {
+    res.render('paperchat', {});
+});
+
+router.get('/scissorschat', (req, res) => {
+    res.render('scissorschat', {});
+});
+
 module.exports = router;
