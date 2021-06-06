b88d45996205cadb024822d418ea6b6258c87683
nmisustin <nmisustin@gmail.com>
2021-06-05 20:24:05 -0400

    bug fix

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { Post } = require('../../../just-tech-new/models');
 const User = require('../../models/User');
 
 router.get('/', (req, res) => {
